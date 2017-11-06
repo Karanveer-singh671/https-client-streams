9cd00b8b3108537f107aa0af23480f5363186135
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-11-06 00:02:43 +0000

    3nd commit

--- https_example.js
+++ https_example.js
@@ -6,3 +6,13 @@ var options = {
   host: 'www.example.org',
   path:'/'
 };
+
+
+var callback = function() {
+  console.log('in response handler callback');
+
+}
+
+console.log('im about to request');
+
+https.request(options, callback).end();
